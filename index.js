12bc3ddffe0147acdea686dfeacd9757cccced22
Michael Mayer <michael@schnittstabil.de>
2016-10-07 20:49:36 +0200

    fix globby errors

--- index.js
+++ index.js
@@ -3,15 +3,7 @@ const path = require('path');
 const arrify = require('arrify');
 const globby = require('globby');
 const cpFile = require('cp-file');
-const NestedError = require('nested-error-stacks');
-
-class CpyError extends NestedError {
-	constructor(message, nested) {
-		super(message, nested);
-		Object.assign(this, nested);
-		this.name = 'CpyError';
-	}
-}
+const CpyError = require('./cpy-error');
 
 const preprocessSrcPath = (srcPath, opts) => opts.cwd ? path.resolve(opts.cwd, srcPath) : srcPath;
 
@@ -39,6 +31,9 @@ module.exports = (src, dest, opts) => {
 	}
 
 	return globby(src, opts)
+		.catch(err => {
+			throw new CpyError(`Cannot glob \`${src}\`: ${err.message}`, err);
+		})
 		.then(files => Promise.all(files.map(srcPath => {
 			const from = preprocessSrcPath(srcPath, opts);
 			const to = preprocessDestPath(srcPath, dest, opts);
@@ -46,8 +41,5 @@ module.exports = (src, dest, opts) => {
 			return cpFile(from, to, opts).catch(err => {
 				throw new CpyError(`Cannot copy from \`${from}\` to \`${to}\`: ${err.message}`, err);
 			});
-		})))
-		.catch(err => {
-			throw new CpyError(`Cannot glob \`${src}\`: ${err.message}`, err);
-		});
+		})));
 };
